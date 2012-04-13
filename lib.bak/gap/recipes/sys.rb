d088d00f00b8aba6313073f1ec240b8ccfce4e92
Takashi Yaguchi <yaguchi@galileoscope.com>
2012-04-13 14:20:44 +0900

    commit

--- lib.bak/gap/recipes/sys.rb
+++
@@ -1,5 +0,0 @@
-namespace :sys do
-  task :ps do
-    run "ps alx | grep unicorn_zebra_beta.rb"
-  end
-end
--- lib.bak/gap/sudo_run.rb
+++
@@ -1,7 +0,0 @@
-module Gap 
-  module SudoRun
-    def sudo_run command
-      run command.gsub(/sudo/,sudo)
-    end 
-  end 
-end
